b08604be899e99cef34adf60ff2c3b0c7c4f1046
vishal5500 <vishalgurav5500@gmail.com>
2024-03-09 17:03:40 +0530

    ASsigment for Data Type
    ASsigment for Data Type

---
+++ 02_typeOf.js
@@ -0,0 +1,48 @@
+console.log("===========Step 1=========");
+
+var bankName;
+console.log("Bank Name:" ,bankName);
+var typeofBank=typeof bankName;
+console.log("Type Of Data:",typeofBank);
+
+console.log("======AFTER INITI=====");
+
+bankName = "SBI Bank";
+console.log("Bank Name:" ,bankName);
+var typeofBank=typeof bankName;
+console.log("Type Of Data:",typeofBank);
+
+console.log("======STEP 2=====");
+
+var marks = '90%'
+console.log("Marks Otained:",marks);
+var typerOfMarks = typeof marks 
+console.log("Data Type of:",typerOfMarks);
+
+console.log("=====STEP 3=====");
+
+var isWorking;
+console.log("Is Working:",isWorking);
+var typeOfWorking = typeof isWorking
+console.log("Type Of Working:",typeOfWorking);
+
+console.log("==AFTER INITI==");
+isWorking = true;
+console.log("Is Working:",isWorking);
+var typeOfWorking = typeof isWorking ;
+console.log("Type Of Working:",typeOfWorking);
+
+console.log("======STEP 4======");
+
+console.log("===A===")
+var totalCount = "Hundred And Seven"
+console.log("The Total cunt is:",totalCount);
+var typerOfTotalCount = typeof totalCount
+console.log("Type of Total Count Is:",typerOfTotalCount);
+console.log("===B===");
+totalCount = 107
+console.log("The Total cunt New value is:",totalCount);
+var NewValue = typeof totalCount
+console.log("The Type Of new Value is:",NewValue);
+
+
